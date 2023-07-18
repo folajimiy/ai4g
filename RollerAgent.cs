18e059944fdb8ab956617f82456b67032554de53
folajimiy <55110562+folajimiy@users.noreply.github.com>
2023-07-18 13:05:15 -0400

    Update RollerAgent.cs

--- RollerAgent.cs
+++ RollerAgent.cs
@@ -1,22 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
 using Unity.MLAgents.Actuators;
 
-public class RollerAgent : Agent
+public class AgentCode: Agent
 {
+
     Rigidbody rBody;
     public Transform Target;
-    public float speed = 10;
+    public float forceMultiplier = 10;
 
-    public override void Initialize()
+    // Start is called before the first frame update
+    void Start()
     {
         rBody = GetComponent<Rigidbody>();
     }
 
     public override void OnEpisodeBegin()
     {
+        // If the Agent fell, zero its momentum
         if (this.transform.localPosition.y < 0)
         {
             this.rBody.angularVelocity = Vector3.zero;
@@ -24,27 +27,70 @@ public class RollerAgent : Agent
             this.transform.localPosition = new Vector3(0, 0.5f, 0);
         }
 
+        // Move the target to a new spot
         Target.localPosition = new Vector3(Random.value * 8 - 4, 0.5f, Random.value * 8 - 4);
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        // Target and Agent positions
+        sensor.AddObservation(Target.localPosition);
+        sensor.AddObservation(this.transform.localPosition);
+
+        // Agent velocity
+        sensor.AddObservation(rBody.velocity.x);
+        sensor.AddObservation(rBody.velocity.z);
     }
 
     public override void OnActionReceived(ActionBuffers actionBuffers)
     {
+        // Actions, size = 2
         Vector3 controlSignal = Vector3.zero;
         controlSignal.x = actionBuffers.ContinuousActions[0];
         controlSignal.z = actionBuffers.ContinuousActions[1];
-        rBody.AddForce(controlSignal * speed);
+        rBody.AddForce(controlSignal * forceMultiplier);
 
+        Debug.Log(actionBuffers.ContinuousActions);
+
+        // Rewards
         float distanceToTarget = Vector3.Distance(this.transform.localPosition, Target.localPosition);
 
+        // Reached target
         if (distanceToTarget < 1.42f)
         {
             SetReward(1.0f);
             EndEpisode();
         }
 
-        if (this.transform.localPosition.y < 0)
+        // Fell off platform
+        else if (this.transform.localPosition.y < 0)
         {
             EndEpisode();
         }
     }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var continuousActionsOut = actionsOut.ContinuousActions;
+        continuousActionsOut[0] = Input.GetAxis("Horizontal");
+        continuousActionsOut[1] = Input.GetAxis("Vertical");
+
+        Debug.Log("Horizontal input: " + Input.GetAxis("Horizontal"));
+        Debug.Log("Vertical input: " + Input.GetAxis("Vertical"));
+    }
+
+
+    //public float speed = 10.0f;
+
+    void FixedUpdate()
+    {
+        // rBody.AddForce(new Vector3(1, 0, 1) * forceMultiplier);
+        /*float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rBody.AddForce(movement * speed);*/
+    }
 }
+
